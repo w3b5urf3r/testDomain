da9d58ee0351b065d3b5ac23569651f7dccd8fe0
mario lopez <mariolopez@marios-MBP.lan>
2017-08-25 12:13:24 +1000

    retrofit http type safe cliente, renaming of ApiManager class

--- app/src/main/java/com/domain/mariolopez/testlist/di/MainComponent.kt
+++ app/src/main/java/com/domain/mariolopez/testlist/di/MainComponent.kt
@@ -1,6 +1,6 @@
 package com.domain.mariolopez.testlist.di
 
-import com.domain.mariolopez.testlist.api.ApiManager
+import com.domain.mariolopez.testlist.api.RestAdapter
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.bind
 import com.github.salomonbrys.kodein.provider
@@ -12,6 +12,6 @@ object MainComponent {
 
     fun Kodein.Builder.TestListComponent() {
         //Api  manager
-        bind<ApiManager>() with provider { ApiManager() }
+        bind<RestAdapter>() with provider { RestAdapter() }
     }
 }
